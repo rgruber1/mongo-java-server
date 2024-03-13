4b3f40aab9bda2e0614c638370529cd00936994a
Robert Gruber <gruber.robert@gmail.com>
2024-03-13 15:17:53 +0000

    Extract mongo version to interface

--- core/src/main/java/de/bwaldvogel/mongo/MongoVersion.java
+++ core/src/main/java/de/bwaldvogel/mongo/MongoVersion.java
@@ -1,12 +1,16 @@
 package de.bwaldvogel.mongo;
 
+import de.bwaldvogel.mongo.backend.Utils;
+
 import java.util.List;
 
 public interface MongoVersion {
 
     List<Integer> getVersionArray();
 
-    String toVersionString();
+    default String toVersionString() {
+        return Utils.join(getVersionArray(), ".");
+    }
 
     int getWireVersion();
 
